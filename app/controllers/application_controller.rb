e98d3c63c58e6e7032c0311cedd5e384c6572e13
Martin Tsai <mtsai@creaxon.com>
2014-01-28 21:28:32 +0800

    disable CSRF for now to accept cross-site ajax request from mobile. Must reenable it later after proper authentication is added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  # FIXME: figure out how to properly submit cross-site ajax POST
+  # from mobile, must reenable this later !!!!
+
+  # protect_from_forgery with: :exception
 end
