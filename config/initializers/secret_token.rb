a650e20dfde693dabc3e1c90ffcf5272faa9b39e
Martin Tsai <mtsai@creaxon.com>
2014-01-28 18:58:19 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scfbe::Application.config.secret_key_base = 'd4bcabb31248f2a1eba4a4f1868d8aad5c99de65ac260d9079c4987719af5a88b969ea2c89ca8143047b162ee1a50ab16ae076482f61add9c27f8ed6acc8357b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scfbe::Application.config.session_store :cookie_store, key: '_scfbe_session'
