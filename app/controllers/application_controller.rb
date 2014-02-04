448c580a78939baf213d691913ab9168fe158bb7
Martin Tsai <mtsai@creaxon.com>
2014-02-04 23:11:47 +0800

    - add forgery protection back to application_controller.rb - set forgery protection to null_session in api/issues_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  # FIXME: figure out how to properly submit cross-site ajax POST
-  # from mobile, must reenable this later !!!!
-
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 end
