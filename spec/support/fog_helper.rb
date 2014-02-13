3312f1974fa701df032705940598e329e0909aef
Martin Tsai <mtsai@creaxon.com>
2014-02-14 03:39:53 +0800

    generalize such that using bucket name other than 'scfbesnapshot' won't break the tests.

--- spec/support/fog_helper.rb
+++ spec/support/fog_helper.rb
@@ -1,4 +1,10 @@
 Fog.mock!
-	# Fog.credentials_path = Rails.root.join('config/fog_credentials.yml')
-	connection = Fog::Storage.new(:provider => 'AWS', :aws_access_key_id => ENV['S3_KEY'], :aws_secret_access_key => ENV['S3_SECRET'],)
-	connection.directories.create(:key => 'scfbesnapshot')
+connection = Fog::Storage.new(
+  provider: 'AWS',
+  aws_access_key_id: ENV['S3_KEY'],
+  aws_secret_access_key: ENV['S3_SECRET'],
+  region: ENV['S3_REGION']
+)
+connection.directories.create(
+  :key => ENV['S3_BUCKET']
+)
--- spec/uploaders/fog_spec.rb
+++ spec/uploaders/fog_spec.rb
@@ -7,22 +7,22 @@ require 'spec_helper'
 # end
 
 
-	describe SnapshotUploader do 
+	describe SnapshotUploader do
 
 		include FakeFS::SpecHelpers
 
 		context 'for non-production environment' do
-			it 'should upload image to scfbesnapshot on s3' do
+			it 'should upload image to designated bucket on s3' do
 				FakeFS.activate!
-				FakeFS::File.should_receive(:chmod) 
+				FakeFS::File.should_receive(:chmod)
 				File.open('test_file', 'w') do |f|
 					f.puts('foo')
 				end
 				uploader_test = Issue.new(title: "something")
 				uploader_test.snapshot = File.open('test_file')
 				uploader_test.save!
-				uploader_test.snapshot.url.should match /.*\/scfbesnapshot.*/
+        uploader_test.snapshot.url.should match /.*\/#{ENV['S3_BUCKET']}.*/
 				FakeFS.deactivate!
 			end
 		end
-end	
+end
