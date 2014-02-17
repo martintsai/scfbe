734024df86369becccaa1f1582faa8b2224b0f0c
sophie <bmzheng@ymail.com>
2014-02-17 22:17:13 +0800

    modification for views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
  
   def index
-    @issues = Issue.all.limit(3)
     if user_signed_in?
       redirect_to issues_path(current_user.issues)
     end 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213124909) do
+ActiveRecord::Schema.define(version: 20140216170037) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -66,11 +66,12 @@ ActiveRecord::Schema.define(version: 20140213124909) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "authentication_token"
     t.string   "provider"
     t.string   "uid"
     t.string   "image"
     t.string   "name"
+    t.string   "authentication_token"
+    t.string   "neighbourhood"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
