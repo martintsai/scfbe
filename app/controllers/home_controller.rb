8224fd9dc0ae1827f09029d4db02edcdb1013c31
Martin Tsai <mtsai@creaxon.com>
2014-02-18 01:57:31 +0800

    issues_path(current_user.issues) resulted in following path on address bar: /issues.%23<ActiveRecord::Associations::CollectionProxy::ActiveRecord_Associations_CollectionProxy_Issue:0x007f83a9e2c050>
    removed argument for now, should discuss what's the desired behavior

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
- 
+
   def index
     if user_signed_in?
-      redirect_to issues_path(current_user.issues)
-    end 
+      # redirect_to issues_path(current_user.issues)
+      redirect_to issues_path
+    end
   end
 
 end
